8b615529b4839007082f3db6692a82667b5fb672
guha-rahul <69rahul16@gmail.com>
2025-02-02 02:54:03 +0530

    init

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -5,18 +5,14 @@ import {
   mainnet,
   optimism,
   polygon,
-  sepolia,
+  sepolia,mode, modeTestnet
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
+  modeTestnet,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
